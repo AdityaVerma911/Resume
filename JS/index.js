d0f92944662596f1f711beb566550683e4653259
AdityaVerma911 <vaditya587@gmail.com>
2021-11-21 18:42:22 +0530

    second commit

--- JS/index.js
+++ JS/index.js
@@ -1,6 +1,30 @@
+const sections = document.querySelectorAll('section');
+const navLi = document.querySelectorAll('.nav .nav-items li a');
+
+window.addEventListener('scroll', ()=>{
+  let current = '';
+
+  sections.forEach(section =>{
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.clientHeight;
+    if(pageYOffset >=(sectionTop - sectionHeight/ 3)){
+      current = section.getAttribute('id');
+    }
+
+  })
+   navLi.forEach(li =>{
+     li.classList.remove('active');
+     if(li.classList.contains(current)){
+       li.classList.add('active');
+     }
+   })
+})
+
+
+
 function toggle_display()
 {
-    var navbar = document.getElementById("navbar");
+    var navbar = document.getElementById("navbar2");
    
     var display_setting = navbar.style.display;
 
@@ -14,3 +38,5 @@ function toggle_display()
       
     }
   }
+
+
